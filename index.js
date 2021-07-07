fcffe55c1eff74ed3a486f0fb279e4998051c581
Ayush4385 <sanchesterstudios@gmail.com>
2021-07-07 12:41:18 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+	console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interaction', async interaction => {
+	if (!interaction.isCommand()) return;
+	if (interaction.commandName === 'ping') {
+		await interaction.reply('Pong!');
+	}
+});
+
+client.login('token');
